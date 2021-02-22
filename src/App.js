37c5c316966f6929493ed574c3c40f4df6de3b2f
Yusa Islam <yusaislam48@gmail.com>
2021-02-22 23:24:09 +0600

    added component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let person = {
+    name: "Dr. Mahfuz",
+    job: "Singer"
+  }
+  let style ={
+    color: "red",
+    backgroundColor: "yellow"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done! <code>src/App.js</code> and save to reload.
-        </p>
-        
+
+        <h1 className= "" style={style}>Name: {person.name + '   |  ' + person.job}</h1>
+        <h4>{person.job}</h4>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  return (
+  <div style={{border: '2px solid yellow', margin:  '10px', padding: '5px'}}>
+    <h1>Name : Sakib</h1>
+    <p>I am hero!</p>
+  </div>
+  )
 }
 
 export default App;
